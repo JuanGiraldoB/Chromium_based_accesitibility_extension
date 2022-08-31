735acd15540951533882e6a78e60f01d10337b09
JuanGiraldoB <jugiraldob@unal.edu.co>
2022-08-31 11:20:05 -0500

    Removed commands

--- background.js
+++ background.js
@@ -1,15 +1,1 @@
 console.log('background running')
-
-chrome.runtime.onMessage.addListener(data => {
-    prueba()
-})
-
-
-chrome.commands.onCommand.addListener(function (command) {
-    if (command === "Ctrl+L") {
-        console.log("Ctrl-L successful.");
-    }
-    else if (command === "Ctrl+M") {
-        console.log("Ctrl+M successful.");
-    }
-});
