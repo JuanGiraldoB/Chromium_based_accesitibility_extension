c3abe7665f9c680689e50907497214b05003e1f0
JuanGiraldoB <jugiraldob@unal.edu.co>
2022-09-07 12:28:53 -0500

    Added default values (line height, font family, text size)

--- background.js
+++ background.js
@@ -1,1 +1,15 @@
-console.log('background running')
+lineHeightValue = 1.5
+textSizeValue = 16
+textFamilyValue = "Arial"
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.local.set({
+        lineHeight: lineHeightValue,
+        textSize:   textSizeValue,
+        textFamily: textFamilyValue
+    }, () => {
+        console.log("Line height: " + lineHeightValue);
+        console.log("Text size: " + textSizeValue);
+        console.log("text family: " + textFamilyValue);
+    });
+})
