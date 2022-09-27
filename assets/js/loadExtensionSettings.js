b2001b990b2e6836d5079044ed7efc1c0240ee7e
JuanGiraldoB <jugiraldob@unal.edu.co>
2022-09-27 10:49:04 -0500

    Changed storage.local to storage.sync

--- assets/js/loadExtensionSettings.js
+++ assets/js/loadExtensionSettings.js
@@ -13,14 +13,28 @@ load.addEventListener("click", async () => {
 function loadSettings() {
     console.log("Loading settings...")
 
-    chrome.storage.local.get(["textSize", "textFamily", "lineHeight"], function (result) {
-        console.log("Current text size: " + result.textSize)
-        console.log("Current text family: " + result.textFamily)
-        console.log("Current line height: " + result.lineHeight)
+    chrome.storage.sync.get(["textSize", "textFamily", "lineHeight", "fontColor", "linksColor", "titleColor"], function (result) {
+        console.log("Current text size: " + result.textSize);
+        console.log("Current text family: " + result.textFamily);
+        console.log("Current line height: " + result.lineHeight);
+        console.log("Current font color: " + result.fontColor);
+        console.log("Current links color: " + result.linksColor);
+        console.log("Current title color: " + result.titleColor);
 
         var tags = document.getElementsByTagName("*");
+        var titles = ["H1","H2","H3","H4","H5"]; // https://stackoverflow.com/questions/2430000/determine-if-string-is-in-list-in-javascript
 
         for (let i = 0; i < tags.length; i++) {
+            // console.log(tags[i].tagName);
+
+            if (result.fontColor !== undefined && tags[i].tagName !== "A" && !titles.includes(tags[i].tagName)) {
+                tags[i].style.color = result.fontColor;
+            }else if (result.linksColor !== undefined && tags[i].tagName === "A"){
+                tags[i].style.color = result.linksColor;
+            }else if (result.titleColor !== undefined && titles.includes(tags[i].tagName)){
+                tags[i].style.color = result.titleColor;
+            }
+
             tags[i].style.fontFamily = result.textFamily;
             tags[i].style.fontSize = result.textSize + "px";
             tags[i].style.lineHeight = result.lineHeight + "";
@@ -30,13 +44,3 @@ function loadSettings() {
     })
 
 }
-
-const openOptions = document.getElementById("goToOptions");
-
-openOptions.addEventListener('click', function () {
-    if (chrome.runtime.openOptionsPage) {
-        chrome.runtime.openOptionsPage();
-    } else {
-        window.open(chrome.runtime.getURL('options.html'));
-    }
-});
--- background.js
+++ background.js
@@ -1,20 +1,23 @@
-var lineHeightValue = 1.5
-var textSizeValue = 16
-var textFamilyValue = "Arial"
-var ttsRunningValue = "false"
-var ttsPauseResumeValue = "pause"
-var displayEnlargeValue = "block"
-var displayReduceValue = "block"
-var displayFontValue = "block"
-var displayIncreaseLineHeightValue = "block"
-var displayReduceLineHeightValue = "block"
-var displayColorPickerValue = "block"
-var displayTTSValue = "block"
-var displayOptionsValue = "block"
-var displayLoadSettingsValue = "block"
+var lineHeightValue = 1.5;
+var textSizeValue = 16;
+var textFamilyValue = "Arial";
+var ttsRunningValue = "false";
+var ttsPauseResumeValue = "pause";
+var displayEnlargeValue = "block";
+var displayReduceValue = "block";
+var displayFontValue = "block";
+var displayIncreaseLineHeightValue = "block";
+var displayReduceLineHeightValue = "block";
+var displayColorPickerValue = "block";
+var displayTTSValue = "block";
+var displayOptionsValue = "block";
+var displayLoadSettingsValue = "block";
+var fontColorValue = undefined;
+var linksColorValue = undefined;
+var titleColorValue = undefined;
 
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.local.set({
+    chrome.storage.sync.set({
         lineHeight: lineHeightValue,
         textSize: textSizeValue,
         textFamily: textFamilyValue,
@@ -28,14 +31,13 @@ chrome.runtime.onInstalled.addListener(() => {
         displayColorPicker: displayColorPickerValue,
         displayTTS: displayTTSValue,
         displayOptions: displayOptionsValue,
-        displayLoadSettings: displayLoadSettingsValue
+        displayLoadSettings: displayLoadSettingsValue,
+        fontColor: fontColorValue,
+        linksColor: linksColorValue,
+        titleColor: titleColorValue
 
     }, () => {
-        console.log("Line height: " + lineHeightValue);
-        console.log("Text size: " + textSizeValue);
-        console.log("text family: " + textFamilyValue);
-        console.log("tts status: " + ttsRunningValue);
-        console.log("tts stop/resume: " + ttsPauseResumeValue);
+        console.log("Default values set");
     });
 })
 
@@ -43,7 +45,7 @@ chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
 
         if (request.msg === "load") {
-            chrome.storage.local.get([
+            chrome.storage.sync.get([
                 "ttsRunning",
                 "ttsPauseResume",
                 "displayEnlarge",
@@ -73,18 +75,18 @@ chrome.runtime.onMessage.addListener(
 
         } else if (request.msg === "save") {
             if (request.ttsRunning) {
-                chrome.storage.local.set({ ttsRunning: request.ttsRunning }, function () {
+                chrome.storage.sync.set({ ttsRunning: request.ttsRunning }, function () {
                     console.log('Running?: ' + request.ttsRunning);
                 });
             } else if (request.ttsPauseResume) {
-                chrome.storage.local.set({ ttsPauseResume: request.ttsPauseResume }, function () {
+                chrome.storage.sync.set({ ttsPauseResume: request.ttsPauseResume }, function () {
                     console.log('Paused?: ' + request.ttsPauseResume);
                 });
             }
 
         }
 
-
         return true; // Ref: https://stackoverflow.com/questions/59914490/how-to-handle-unchecked-runtime-lasterror-the-message-port-closed-before-a-res
     }
 );
+
