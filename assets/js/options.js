41cb1b4b6ef81fa4b23da7d52449fffe0fe0eb40
JuanGiraldoB <jugiraldob@unal.edu.co>
2022-10-03 16:54:29 -0500

    Added tts rate/pitch/voice selector

--- assets/js/options.js
+++ assets/js/options.js
@@ -1,7 +1,11 @@
-const saveBtn = document.getElementById("save")
+speechSynthesis.addEventListener("voiceschanged", () => {
+    console.log("Voices loaded: ", speechSynthesis.getVoices().length);
+});
+
+const saveBtn = document.getElementById("save");
 
 saveBtn.addEventListener("click", () => {
-    chrome.storage.local.set({
+    chrome.storage.sync.set({
         displayEnlarge: isChecked("checkEnlarge") + "",
         displayReduce: isChecked("checkReduce") + "",
         displayIncreaseLineHeight: isChecked("checkIncreaseLine") + "",
@@ -10,7 +14,8 @@ saveBtn.addEventListener("click", () => {
         displayFont: isChecked("checkFont") + "",
         displayTTS: isChecked("checkTTS") + "",
         displayLoadSettings: isChecked("checkLoadSettings") + "",
-        displayOptions: isChecked("checkOptions") + ""
+        displayOptions: isChecked("checkOptions") + "",
+        displayInvertColor: isChecked("checkInvertColor")
     });
 });
 
@@ -19,7 +24,7 @@ function isChecked(id) {
 }
 
 function restoreOptions() {
-    chrome.storage.local.get([
+    chrome.storage.sync.get([
         "displayEnlarge",
         "displayReduce",
         "displayFont",
@@ -28,7 +33,8 @@ function restoreOptions() {
         "displayColorPicker",
         "displayTTS",
         "displayOptions",
-        "displayLoadSettings"],
+        "displayLoadSettings",
+        "displayInvertColor"],
         function (result) {
             document.getElementById("checkEnlarge").checked = isDisplayed(result.displayEnlarge),
                 document.getElementById("checkReduce").checked = isDisplayed(result.displayReduce),
@@ -38,12 +44,43 @@ function restoreOptions() {
                 document.getElementById("checkFont").checked = isDisplayed(result.displayFont),
                 document.getElementById("checkTTS").checked = isDisplayed(result.displayTTS),
                 document.getElementById("checkLoadSettings").checked = isDisplayed(result.displayLoadSettings),
-                document.getElementById("checkOptions").checked = isDisplayed(result.displayOptions)
+                document.getElementById("checkOptions").checked = isDisplayed(result.displayOptions),
+                document.getElementById("checkInvertColor").checked = isDisplayed(result.displayInvertColor)
         });
 }
 
 function isDisplayed(value) {
     return value == "block" ? true : false;
+}
+
+const ttsPitch = document.getElementById("ttsPitch");
+const ttsSpeed = document.getElementById("ttsSpeed");
+const ttsGenre = document.querySelectorAll(".ttsVoiceGenere");
+
+ttsGenre.forEach((e) => {
+    e.addEventListener("click", testTTSParams.bind(null, e, undefined, undefined))
+});
+ttsSpeed.addEventListener("change", testTTSParams.bind(null, undefined, ttsSpeed, undefined));
+ttsPitch.addEventListener("change", testTTSParams.bind(null, undefined, undefined, ttsPitch));
+
+function testTTSParams(genre, speed, pitch){
+    const synth = window.speechSynthesis;
+    const voices = synth.getVoices();
+    var msg = new SpeechSynthesisUtterance();
+
+    msg.text = "This is a test";
+    msg.voice = genre ? voices[genre.value] : voices[2];
+    msg.rate = speed ? speed.value : 1; // Between 0.1 and 10
+    msg.pitch = pitch ? pitch.value : 1; // Between 1 and 2
+
+    synth.speak(msg);
+
+    let interval = setInterval(() => {
+        if (!speechSynthesis.speaking) {
+            synth.cancel();
+            clearInterval(interval);
+        }
+    }, 1000);
 }
 
 document.addEventListener('DOMContentLoaded', restoreOptions);
